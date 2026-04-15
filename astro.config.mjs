9192ef2783ff5f7ac647885e913b491d033aa74e
waynehet <waynehet@gmail.com>
2026-04-15 12:04:58 -0400

    Fix all internal links to include /classz2 base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://waynehet.github.io',
-  base: '/',
+  base: '/classz2',
   vite: {
     plugins: [tailwindcss()]
   },
